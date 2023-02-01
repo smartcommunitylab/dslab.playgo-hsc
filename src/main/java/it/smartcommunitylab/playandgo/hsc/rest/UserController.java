0c8516f35480455b728829736a7343e015fe4a9f
micnori <micnori@users.noreply.github.com>
2023-02-01 09:53:13 +0100

    minor fix

--- src/main/java/it/smartcommunitylab/playandgo/hsc/rest/UserController.java
+++ src/main/java/it/smartcommunitylab/playandgo/hsc/rest/UserController.java
@@ -2,6 +2,8 @@ package it.smartcommunitylab.playandgo.hsc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +16,12 @@ public class UserController {
 	@Autowired
 	UserConsentService userConsentService;
 	
+	@GetMapping("/api/user/consent")
 	public ResponseEntity<UserConsent> getUserConsent() {
 		return ResponseEntity.ok(userConsentService.getUserConsent());
 	}
 	
+	@PutMapping("/api/user/consent")
 	public ResponseEntity<UserConsent> updateConsent(@RequestParam boolean privacy, @RequestParam boolean termOfConditions) {
 		return ResponseEntity.ok(userConsentService.updateConsent(privacy, termOfConditions));
 	}
